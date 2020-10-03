acbfeb779a7d446c9475c56123bc1669868a92c3
Zakhar Vorsulev <vorsulev@gmail.com>
2020-10-02 22:53:42 -0400

    Added NuN default velue

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
-let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
-alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
-let answerNumber  = Math.floor((minValue + maxValue) / 2);
-let orderNumber = 1;
-let gameRun = true;
+let minValue = parseInt(prompt('Минимальное знание числа для игры','0')) || 0
+let maxValue = parseInt(prompt('Максимальное знание числа для игры','100')) || 100
+alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`)
+let answerNumber  = Math.floor((minValue + maxValue) / 2)
+let orderNumber = 1
+let gameRun = true
 let victory = [`Я всегда угадываю\n\u{1F60E}`, `Какой я молодец!\n\u{1F60A}`, `Это было не трудно!\n\u{1F60E}`]
 let answerPhrase = [`Вы загадали неправильное число!\n\u{1F914}`, `Я сдаюсь..\n\u{1F92F}` , `Ну так же нельзя!\n\u{1F624}`]
 let guess = [`Вы загадали число` , `Должно быть это` , `A как насчет`]
 
 
-const orderNumberField = document.getElementById('orderNumberField');
-const answerField = document.getElementById('answerField');
+const orderNumberField = document.getElementById('orderNumberField')
+const answerField = document.getElementById('answerField')
 
-orderNumberField.innerText = orderNumber;
-answerField.innerText = `Вы загадали число ${answerNumber }?`;
+orderNumberField.innerText = orderNumber
+answerField.innerText = `Вы загадали число ${answerNumber }?`
 
 document.getElementById('btnRetry').addEventListener('click', function () {
     window.location.reload()
-    minValue = 0;
-    maxValue = 100;
-    orderNumber = 0;
+    minValue = 0
+    maxValue = 100
+    orderNumber = 0
 })
-
-if (minValue === NaN){
-    minValue = 0;
-}
-
 
 document.getElementById('btnOver').addEventListener('click', function () {
     if (gameRun){
@@ -34,10 +29,10 @@ document.getElementById('btnOver').addEventListener('click', function () {
             answerField.innerText = a
             gameRun = false
         } else {
-            minValue = answerNumber  + 1;
-            answerNumber  = Math.floor((minValue + maxValue) / 2);
-            orderNumber++;
-            orderNumberField.innerText = orderNumber;
+            minValue = answerNumber  + 1
+            answerNumber  = Math.floor((minValue + maxValue) / 2)
+            orderNumber++
+            orderNumberField.innerText = orderNumber
             var g = guess[ Math.floor( Math.random() * answerPhrase.length) ]
             answerField.innerText = `${g} ${answerNumber }?`
         }
@@ -49,12 +44,12 @@ document.getElementById('btnLess').addEventListener('click', function () {
         if (minValue === maxValue || minValue == answerNumber){
             var a = answerPhrase[ Math.floor( Math.random() * answerPhrase.length) ]
             answerField.innerText = a
-            gameRun = false;
+            gameRun = false
         } else {
-            maxValue = answerNumber  - 1;
-            answerNumber  = Math.floor((minValue + maxValue) / 2);
-            orderNumber++;
-            orderNumberField.innerText = orderNumber;
+            maxValue = answerNumber  - 1
+            answerNumber  = Math.floor((minValue + maxValue) / 2)
+            orderNumber++
+            orderNumberField.innerText = orderNumber
             var g = guess[ Math.floor( Math.random() * answerPhrase.length) ]
             answerField.innerText = `${g} ${answerNumber }?`
         }
@@ -65,7 +60,7 @@ document.getElementById('btnEqual').addEventListener('click', function () {
     if (gameRun){
         var v = victory[ Math.floor( Math.random() * victory.length) ]
         answerField.innerText = v
-        gameRun = false;
+        gameRun = false
     }
 })
 
